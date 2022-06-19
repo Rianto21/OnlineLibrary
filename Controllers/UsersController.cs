556808c036e1be223ef3d63f72f8bd3842e8fd6a
Oryto Mikli <orytomikli21@gmail.com>
2022-06-19 23:44:02 +0700

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineLibrary.Controllers
+{
+    public class UsersController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Books
+    {
+        public int _id { get; set; }
+        public string? Judul { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime TanggalTerbit { get; set; }
+        public string? Jenis { get; set; }
+        public string Penulis { get; set; }
+        public int JumlahHalaman { get; set; }
+        public Boolean StatusKetersediaan { get; set; }
+
+
+    }
+}
